ead4b1ec195e83fcfa73b0d958c66281449a2ab8
roma12342 <zombimen12@gmail.com>
2025-11-23 11:53:10 +0300

    Add files via upload
    v1.0

---
+++ ping_pong.py
@@ -0,0 +1,68 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image,(self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+      
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+       
+
+back = (200,255,255)
+win_height = 500
+win_width = 600
+
+window = display.set_mode((win_height, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+rocket1 = Player('raketka.png',30,200,50,150,4)
+rocket2 = Player('raketka2.png',420,200,50,150,4)
+ball = GameSprite('ball1.png',200,200,50,50,4)
+
+speed_x = 3
+speed_y = 3
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if not finish:
+        window.fill(back)  
+
+        rocket1.update_l()
+        rocket2.update_r()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        rocket1.reset()
+        rocket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
